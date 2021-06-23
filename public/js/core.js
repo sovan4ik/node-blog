64932b89cac118dd40697c0465f489fecb9ba74a
sovan4ik <salimonenko.alex@gmail.com>
2021-06-23 22:13:37 +0300

    update add post

--- public/js/core.js
+++ public/js/core.js
@@ -2,7 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('select');
     var instances = M.FormSelect.init(elems,{});
   });
-
+  
+  $(document).ready(function() {
+    $('#post_description').characterCounter();
+  });
 //   $('.delete-post').on('click',  function(e){
 //     e.preventDefault();
 //     var id = $(this).parent().data('id');
